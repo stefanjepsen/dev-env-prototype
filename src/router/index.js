9e15ff780e479752c5e132629e0a1920fba4c9c6
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2021-06-03 01:54:46 +0200

    Final Touch Ups

--- src/router/index.js
+++ src/router/index.js
@@ -21,16 +21,6 @@ const routes = [
       return import(/* webpackChunkName: "about" */ '../views/Register.vue')
     }
   },
-  {
-    path: '/NameSim',
-    name: 'NameSim',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/NameSim.vue')
-    }
-  }
 ]
 
 const router = new VueRouter({
