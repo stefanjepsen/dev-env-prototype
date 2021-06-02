64646a5db5c5e64e1a055e6e5e7aeb74c15d4ab5
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2021-06-02 23:13:05 +0200

    Added test for register with random Email

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,11 +4,14 @@ describe('Check correct h1 on frontpage and if button is pressed', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/dev-env-prototype/')
     cy.contains('h1', 'Games List From API')
+    
   })
 
-  it('changes conditional rendering when button is pressed', () => {
+  it('Checks to see if we get some data from the API', () => {
     cy.get('#fetchData').click();
     cy.contains('h3', 'GamesList');
+    cy.stub()
+   
   })
 })
 
@@ -17,13 +20,15 @@ describe('Check correct h1 on frontpage and if button is pressed', () => {
 describe('Register test', () => {
 
   it('Visits register page', () => {
+    
     cy.get('#toRegister').click();
     cy.get('.action-email')
-      .type('fake@email.com')
+      .type(Math.round(Math.random()*100000)+"@email.com")
     cy.get('.action-name')
-      .type('fake@email.com')
+      .type('Allan Simonsen')
     cy.get('.action-pw')
       .type('fake@email.com')
+      cy.url().should('include', 'register')
     cy.get('#submitButton').click();
 
   })
